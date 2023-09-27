84b5223ec6a6a749889776c3dcfceebb5fce0bc0
Alexandre Samson <alexander_samson@icloud.com>
2023-09-27 12:25:55 +0200

    fragment_main.xml

--- app/src/main/java/com/example/wiindi/fragments/MainFragment.kt
+++ app/src/main/java/com/example/wiindi/fragments/MainFragment.kt
@@ -1,16 +1,22 @@
 package com.example.wiindi.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.wiindi.R
 import com.example.wiindi.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
 
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
@@ -19,6 +25,25 @@ class MainFragment : Fragment() {
     ): View? {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+    //Prüfe ob Location erlaubt ist
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+    //Prüfe ob Location erlaubt ist
+    private fun checkPermission() {
+        if (isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
