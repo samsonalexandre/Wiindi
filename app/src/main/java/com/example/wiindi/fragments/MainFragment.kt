9bd10d701a68c34de36f7528e29bf7663e7bdee2
Alexandre Samson <alexander_samson@icloud.com>
2023-09-27 15:51:02 +0200

    DaysFragment.kt, HoursFragment.kt, VpAdapter.kt

--- app/src/main/java/com/example/wiindi/fragments/MainFragment.kt
+++ app/src/main/java/com/example/wiindi/fragments/MainFragment.kt
@@ -10,12 +10,23 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
 import com.example.wiindi.R
+import com.example.wiindi.adapters.VpAdapter
 import com.example.wiindi.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class MainFragment : Fragment() {
 
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Stunden",
+        "Tage"
+    )
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -30,7 +41,18 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
     }
+
+    private fun init() = with(binding) {
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp) {
+            tab, pos -> tab.text = tList[pos]
+        }.attach()
+    }
+
+
     //Prüfe ob Location erlaubt ist
     private fun permissionListener() {
         pLauncher = registerForActivityResult(
