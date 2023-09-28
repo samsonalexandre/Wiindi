1da0fc958d5c25e46c74af8f591c56703c19f26d
Alexandre Samson <alexander_samson@icloud.com>
2023-09-28 11:37:59 +0200

    list_item.xml, WeatherModel.kt

--- app/src/main/java/com/example/wiindi/MainViewModel.kt
+++ app/src/main/java/com/example/wiindi/MainViewModel.kt
@@ -2,9 +2,10 @@ package com.example.wiindi
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.wiindi.adapters.WeatherModel
 
 class MainViewModel: ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 
 }
