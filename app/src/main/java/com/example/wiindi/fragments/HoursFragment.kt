7cdc40d4f831708219b550eed38cc96156ccce18
Alexandre Samson <alexander_samson@icloud.com>
2023-09-29 09:44:32 +0200

    API Call OK

--- app/src/main/java/com/example/wiindi/fragments/HoursFragment.kt
+++ app/src/main/java/com/example/wiindi/fragments/HoursFragment.kt
@@ -5,18 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wiindi.R
+import com.example.wiindi.adapters.WeatherAdapter
+import com.example.wiindi.adapters.WeatherModel
+import com.example.wiindi.databinding.FragmentHoursBinding
 
 
 class HoursFragment : Fragment() {
-
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding){
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+            WeatherModel(
+                "","12:00",
+                "Sonnig","25ºC",
+                "", "", "",""),
+            WeatherModel(
+                "","13:00",
+                "Sonnig","27ºC",
+                "", "", "",""),
+            WeatherModel(
+                "","14:00",
+                "Sonnig","35ºC",
+                "", "", "","")
+        )
+        adapter.submitList(list)
     }
 
     companion object {
--- app/src/main/java/com/example/wiindi/fragments/MainFragment.kt
+++ app/src/main/java/com/example/wiindi/fragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.wiindi.fragments
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +12,22 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.wiindi.MainViewModel
 import com.example.wiindi.R
 import com.example.wiindi.adapters.VpAdapter
 import com.example.wiindi.databinding.FragmentMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.material.tabs.TabLayoutMediator
 
-
+const val API_KEY = "d57636e8b33d45d79ab155621232609"
 class MainFragment : Fragment() {
+
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+    private val model : MainViewModel by activityViewModels()
 
     private val fList = listOf(
         HoursFragment.newInstance(),
@@ -42,6 +52,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
+        requestWeatherData("Neuss")
     }
 
     private fun init() = with(binding) {
@@ -66,6 +77,28 @@ class MainFragment : Fragment() {
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun requestWeatherData(city: String) {
+        val url = "http://api.weatherapi.com/v1/forecast.json?key= + " +
+                API_KEY +
+                "&q= + " +
+                city +
+                "&days= + " +
+                "4 + " +
+                "&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                result ->Log.d("MyLog", "Error: $result")
+            },
+            {
+                error -> Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(request)
     }
 
     companion object {
